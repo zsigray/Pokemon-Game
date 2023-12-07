52742e28db579bd0dbe0ec038f03c6afde5deab1
Zsofi Zsigray <141722724+zsigray@users.noreply.github.com>
2023-12-07 10:57:50 +0100

    Pokemon capturing done

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,28 +1,35 @@
+import { useState, useEffect } from "react";
+import "./pokemon.css";
+import PokemonChooser from "./PokemonChooser";
 
-import { useState, useEffect } from 'react';
-import './pokemon.css';
-import PokemonChooser from './PokemonChooser';
-
-export default function Pokemon({ location, setLocation, onBackButtonClick }) {
+export default function Pokemon({
+  location,
+  setLocation,
+  onBackButtonClick,
+  usersPokemonsURL,
+  setUsersPokemonsURL,
+}) {
   const [pokemon, setPokemon] = useState(null);
   const [loaded, setLoaded] = useState(false);
   const [pokemonData, setPokemonData] = useState(null);
-  const [type, setType] = useState('');
-
+  const [type, setType] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         if (location.areas.length === 0) {
-          setLoaded(true)
-          return
+          setLoaded(true);
+          return;
         }
         const areaIndex = randomIntFromInterval(0, location.areas.length - 1);
         const areaUrl = location.areas[areaIndex].url;
         const areaData = await fetch(areaUrl);
         const areaJson = await areaData.json();
 
-        const pokemonIndex = randomIntFromInterval(0, areaJson.pokemon_encounters.length - 1);
+        const pokemonIndex = randomIntFromInterval(
+          0,
+          areaJson.pokemon_encounters.length - 1
+        );
         const randomPokemon = areaJson.pokemon_encounters[pokemonIndex];
 
         const pokemonData = await fetch(randomPokemon.pokemon.url);
@@ -33,103 +40,172 @@ export default function Pokemon({ location, setLocation, onBackButtonClick }) {
 
         setPokemon(pokemonJson);
         setPokemonData(pokemonMoreDataJson);
-        setType(pokemonJson.types[0].type.name)
-        console.log(pokemonJson.types[0].type.name)
+        setType(pokemonJson.types[0].type.name);
         setLoaded(true);
       } catch (error) {
-        console.error(error)
+        console.error(error);
       }
     };
-    fetchData()
+    fetchData();
   }, []);
 
   const getTypeClassNameOrImg = (css, type) => {
     switch (type) {
-      case 'fire':
-        return css ? 'nameimageinfo card-background-fire' : 'https://cdn.discordapp.com/attachments/835821286205947924/1181951803831623720/fire_type_symbol_tcg_by_jormxdos_dfgdd9h-fullview.png';
-      case 'water':
-        return css ? 'nameimageinfo card-background-water' : 'https://cdn.discordapp.com/attachments/835821286205947924/1181951307058266195/water_type_symbol_tcg_by_jormxdos_dfgdd9x-fullview.png';
-      case 'grass':
-        return css ?'nameimageinfo card-background-grass' : 'https://cdn.discordapp.com/attachments/835821286205947924/1181951948153421905/grass_type_symbol_tcg_by_jormxdos_dfgdda7-fullview.png';
-      case 'electric':
-        return css ? 'nameimageinfo card-background-electric' : 'https://cdn.discordapp.com/attachments/835821286205947924/1181952528246648832/lightning_type_symbol_tcg_by_jormxdos_dfgddau-pre.png';
-      case 'fairy':
-        return css ? 'nameimageinfo card-background-fairy' : 'https://cdn.discordapp.com/attachments/835821286205947924/1181952805313986630/fairy_type_symbol_tcg_by_jormxdos_dfgdddf-fullview.png';
-      case 'rock':
-        return css ? 'nameimageinfo card-background-rock' : 'https://cdn.discordapp.com/attachments/835821286205947924/1181954018227015730/1200px-PokC3A9mon_Rock_Type_Icon.png';
-      case 'bug':
-        return css ? 'nameimageinfo card-background-bug' : 'https://cdn.discordapp.com/attachments/835821286205947924/1181954225551450143/image-removebg-preview.png';
-      case 'ghost':
-        return css ? 'nameimageinfo card-background-ghost' : 'https://cdn.discordapp.com/attachments/835821286205947924/1181954579282268220/image-removebg-preview_1.png';
-      case 'normal':
-        return css ? 'nameimageinfo card-background-normal' : 'https://cdn.discordapp.com/attachments/835821286205947924/1181954734622515322/colorless_type_symbol_tcg_by_jormxdos_dfgdd92-fullview.png';
-      case 'fighting':
-        return css ? 'nameimageinfo card-background-fighting' : 'https://cdn.discordapp.com/attachments/835821286205947924/1181953284739706981/fighting_type_symbol_tcg_by_jormxdos_dfgddb7-fullview.png';
-      case 'psychic':
-        return css ? 'nameimageinfo card-background-psychic' : 'https://cdn.discordapp.com/attachments/835821286205947924/1181953469687529482/psychic_type_symbol_tcg_by_jormxdos_dfgddbk-fullview.png';
-      case 'steel':
-        return css ? 'nameimageinfo card-background-steel' : 'https://cdn.discordapp.com/attachments/835821286205947924/1181954878755573840/metal_type_symbol_tcg_by_jormxdos_dfgddcz-fullview.png';
-      case 'dark':
-        return css ? 'nameimageinfo card-background-dark' : 'https://cdn.discordapp.com/attachments/835821286205947924/1181953801582821387/darkness_type_symbol_tcg_by_jormxdos_dfgddck-fullview.png';
-      case 'dragon':
-        return css ? 'nameimageinfo card-background-dragon' : 'https://cdn.discordapp.com/attachments/835821286205947924/1181953620858650749/dragon_type_symbol_tcg_by_jormxdos_dfgddc1-fullview.png';
-      case 'flying':
-        return css ? 'nameimageinfo card-background-flying' : 'https://cdn.discordapp.com/attachments/835821286205947924/1181955095978594314/weeFmVwtaMCQAAAABJRU5ErkJggg.png';
-      case 'ground':
-        return css ? 'nameimageinfo card-background-ground' : 'https://upload.wikimedia.org/wikipedia/commons/8/8d/Pok%C3%A9mon_Ground_Type_Icon.svg';
-      case 'poison':
-        return css ? 'nameimageinfo card-background-poison' : 'https://cdn.discordapp.com/attachments/835821286205947924/1181955660477382666/wpHxPSuxRZXAAAAABJRU5ErkJggg.png';
-      case 'ice':
-        return css ? 'nameimageinfo card-background-ice' : 'https://cdn.discordapp.com/attachments/835821286205947924/1181955938052223046/A3vtUPUgNEIZAAAAAElFTkSuQmCC.png';
+      case "fire":
+        return css
+          ? "nameimageinfo card-background-fire"
+          : "https://cdn.discordapp.com/attachments/835821286205947924/1181951803831623720/fire_type_symbol_tcg_by_jormxdos_dfgdd9h-fullview.png";
+      case "water":
+        return css
+          ? "nameimageinfo card-background-water"
+          : "https://cdn.discordapp.com/attachments/835821286205947924/1181951307058266195/water_type_symbol_tcg_by_jormxdos_dfgdd9x-fullview.png";
+      case "grass":
+        return css
+          ? "nameimageinfo card-background-grass"
+          : "https://cdn.discordapp.com/attachments/835821286205947924/1181951948153421905/grass_type_symbol_tcg_by_jormxdos_dfgdda7-fullview.png";
+      case "electric":
+        return css
+          ? "nameimageinfo card-background-electric"
+          : "https://cdn.discordapp.com/attachments/835821286205947924/1181952528246648832/lightning_type_symbol_tcg_by_jormxdos_dfgddau-pre.png";
+      case "fairy":
+        return css
+          ? "nameimageinfo card-background-fairy"
+          : "https://cdn.discordapp.com/attachments/835821286205947924/1181952805313986630/fairy_type_symbol_tcg_by_jormxdos_dfgdddf-fullview.png";
+      case "rock":
+        return css
+          ? "nameimageinfo card-background-rock"
+          : "https://cdn.discordapp.com/attachments/835821286205947924/1181954018227015730/1200px-PokC3A9mon_Rock_Type_Icon.png";
+      case "bug":
+        return css
+          ? "nameimageinfo card-background-bug"
+          : "https://cdn.discordapp.com/attachments/835821286205947924/1181954225551450143/image-removebg-preview.png";
+      case "ghost":
+        return css
+          ? "nameimageinfo card-background-ghost"
+          : "https://cdn.discordapp.com/attachments/835821286205947924/1181954579282268220/image-removebg-preview_1.png";
+      case "normal":
+        return css
+          ? "nameimageinfo card-background-normal"
+          : "https://cdn.discordapp.com/attachments/835821286205947924/1181954734622515322/colorless_type_symbol_tcg_by_jormxdos_dfgdd92-fullview.png";
+      case "fighting":
+        return css
+          ? "nameimageinfo card-background-fighting"
+          : "https://cdn.discordapp.com/attachments/835821286205947924/1181953284739706981/fighting_type_symbol_tcg_by_jormxdos_dfgddb7-fullview.png";
+      case "psychic":
+        return css
+          ? "nameimageinfo card-background-psychic"
+          : "https://cdn.discordapp.com/attachments/835821286205947924/1181953469687529482/psychic_type_symbol_tcg_by_jormxdos_dfgddbk-fullview.png";
+      case "steel":
+        return css
+          ? "nameimageinfo card-background-steel"
+          : "https://cdn.discordapp.com/attachments/835821286205947924/1181954878755573840/metal_type_symbol_tcg_by_jormxdos_dfgddcz-fullview.png";
+      case "dark":
+        return css
+          ? "nameimageinfo card-background-dark"
+          : "https://cdn.discordapp.com/attachments/835821286205947924/1181953801582821387/darkness_type_symbol_tcg_by_jormxdos_dfgddck-fullview.png";
+      case "dragon":
+        return css
+          ? "nameimageinfo card-background-dragon"
+          : "https://cdn.discordapp.com/attachments/835821286205947924/1181953620858650749/dragon_type_symbol_tcg_by_jormxdos_dfgddc1-fullview.png";
+      case "flying":
+        return css
+          ? "nameimageinfo card-background-flying"
+          : "https://cdn.discordapp.com/attachments/835821286205947924/1181955095978594314/weeFmVwtaMCQAAAABJRU5ErkJggg.png";
+      case "ground":
+        return css
+          ? "nameimageinfo card-background-ground"
+          : "https://upload.wikimedia.org/wikipedia/commons/8/8d/Pok%C3%A9mon_Ground_Type_Icon.svg";
+      case "poison":
+        return css
+          ? "nameimageinfo card-background-poison"
+          : "https://cdn.discordapp.com/attachments/835821286205947924/1181955660477382666/wpHxPSuxRZXAAAAABJRU5ErkJggg.png";
+      case "ice":
+        return css
+          ? "nameimageinfo card-background-ice"
+          : "https://cdn.discordapp.com/attachments/835821286205947924/1181955938052223046/A3vtUPUgNEIZAAAAAElFTkSuQmCC.png";
 
       default:
-        return 'nameimageinfo card-background-gray';
+        return "nameimageinfo card-background-gray";
     }
   };
 
   if (loaded && !pokemon) {
     return (
-    <div>
-      <h1>This location doesn't seem to have any pokemon 😢</h1>
-      <button onClick={onBackButtonClick}>Back</button>
-    </div>);
+      <div>
+        <h1>This location doesn't seem to have any pokemon 😢</h1>
+        <button onClick={onBackButtonClick}>Back</button>
+      </div>
+    );
   } else if (!loaded) {
     return <div>Loading...</div>;
   } else {
     return (
       <>
-        <PokemonChooser pokemon={pokemon} onBattleEnd={setLocation} getTypeClassNameOrImg={getTypeClassNameOrImg}/>
+        <PokemonChooser
+          pokemon={pokemon}
+          onBattleEnd={setLocation}
+          getTypeClassNameOrImg={getTypeClassNameOrImg}
+          usersPokemonsURL={usersPokemonsURL}
+          setUsersPokemonsURL={setUsersPokemonsURL}
+        />
         <div className="enemySide">
           <div className={getTypeClassNameOrImg(true, type)}>
-            <table className='nameheaders'>
+            <table className="nameheaders">
               <tr>
-                <td className='basic' colSpan={3}>Basic Pokemon</td>
+                <td className="basic" colSpan={3}>
+                  Basic Pokemon
+                </td>
               </tr>
               <tr>
-                <td className='pokemon-name'>{pokemon.name}</td>
-                <td className='pokemon-hp'>HP{pokemon.stats[0].base_stat}</td>
-                <td className='pokemon-type-icon'><img src={getTypeClassNameOrImg(false, type)} alt="" width={30} height={30} /></td>
+                <td className="pokemon-name">{pokemon.name}</td>
+                <td className="pokemon-hp">HP{pokemon.stats[0].base_stat}</td>
+                <td className="pokemon-type-icon">
+                  <img
+                    src={getTypeClassNameOrImg(false, type)}
+                    alt=""
+                    width={30}
+                    height={30}
+                  />
+                </td>
               </tr>
             </table>
-            <img className='headshot' src={pokemon.sprites.other['official-artwork'].front_shiny} alt={pokemon.name} />
-            <p className='description'>{pokemonData.genera[7].genus}. Length: {pokemon.height * 10}CM Weight: {pokemon.weight / 10}KG</p>
-            <div className='infoonly'>
-              <table className='stats'>
+            <img
+              className="headshot"
+              src={pokemon.sprites.other["official-artwork"].front_shiny}
+              alt={pokemon.name}
+            />
+            <p className="description">
+              {pokemonData.genera[7].genus}. Length: {pokemon.height * 10}CM
+              Weight: {pokemon.weight / 10}KG
+            </p>
+            <div className="infoonly">
+              <table className="stats">
                 <tr>
-                  <td><span className='label'>Attack</span> <span className='labeltext'> </span></td>
-                  <td className='damage'>{pokemon.stats[1].base_stat}</td>
+                  <td>
+                    <span className="label">Attack</span>{" "}
+                    <span className="labeltext"> </span>
+                  </td>
+                  <td className="damage">{pokemon.stats[1].base_stat}</td>
                 </tr>
               </table>
               <hr />
-              <table className='stats'>
+              <table className="stats">
                 <tr>
-                  <td><span className='label'>Defense</span> <span className='labeltext'> </span></td>
-                  <td className='damage'>{pokemon.stats[3].base_stat}</td>
+                  <td>
+                    <span className="label">Defense</span>{" "}
+                    <span className="labeltext"> </span>
+                  </td>
+                  <td className="damage">{pokemon.stats[3].base_stat}</td>
                 </tr>
               </table>
             </div>
-            <p className='italicize'>{pokemonData.flavor_text_entries[0].flavor_text}</p>
-            <p className='bottomtext'><strong>Illus. Mitsuhiro Arita</strong>  C 1995, 96, 98 Nintendo Creatures, GAMEFREAK C 1999 Wizards.  <strong>58/102 o</strong></p>
+            <p className="italicize">
+              {pokemonData.flavor_text_entries[0].flavor_text}
+            </p>
+            <p className="bottomtext">
+              <strong>Illus. Mitsuhiro Arita</strong> C 1995, 96, 98 Nintendo
+              Creatures, GAMEFREAK C 1999 Wizards. <strong>58/102 o</strong>
+            </p>
           </div>
         </div>
       </>
--- src/components/PokemonChooser.jsx
+++ src/components/PokemonChooser.jsx
@@ -1,19 +1,20 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from "react";
-import './pokemon.css';
+import "./pokemon.css";
 
-export default function PokemonChooser({pokemon, onBattleEnd, getTypeClassNameOrImg }){
+export default function PokemonChooser({
+  pokemon,
+  onBattleEnd,
+  getTypeClassNameOrImg,
+  usersPokemonsURL,
+  setUsersPokemonsURL
+}) {
+  const [fighterPokemon, setFighterPokemon] = useState(null);
+  const [usersPokemons, setUsersPokemons] = useState([]);
 
-   const [fighterPokemon, setFighterPokemon] = useState(null);
-   const [usersPokemons, setUsersPokemons] = useState([]);
-   
-   useEffect(() => {
+  useEffect(() => {
     let raceConditionHandler = true;
-    const usersPokemonsURL = [
-        "https://pokeapi.co/api/v2/pokemon/bulbasaur",
-        "https://pokeapi.co/api/v2/pokemon/charizard",
-        "https://pokeapi.co/api/v2/pokemon/poliwhirl"
-    ];
+
     const fetchPokemons = async () => {
       const userPokemonList = [];
       for (const url of usersPokemonsURL) {
@@ -26,102 +27,178 @@ export default function PokemonChooser({pokemon, onBattleEnd, getTypeClassNameOr
       }
     };
     fetchPokemons();
-
     return () => {
       raceConditionHandler = false;
     };
-  }, []);
+    
+  }, [usersPokemonsURL]);
 
-function chooseFighterPokemon(pokemon) {
+  function chooseFighterPokemon(pokemon) {
     setFighterPokemon(pokemon);
-} 
+  }
 
-const usersPokemonsDisplay = usersPokemons.map((ourPokemon) =>{
+  const usersPokemonsDisplay = usersPokemons.map((ourPokemon) => {
     return (
-        <div className="miniPokemon" key={ourPokemon.name}>
+      <div className="miniPokemon" key={ourPokemon.name}>
         <h3>{ourPokemon.name}</h3>
-        <img src={ourPokemon.sprites.other['official-artwork'].front_shiny} alt={ourPokemon.name} width={'100px'} />
-        <button onClick={() => chooseFighterPokemon(ourPokemon)}> Fight with {ourPokemon.name} </button>      
-        </div>
-    )
-});   
+        <img
+          src={ourPokemon.sprites.other["official-artwork"].front_shiny}
+          alt={ourPokemon.name}
+          width={"100px"}
+        />
+        
+        <button onClick={() => chooseFighterPokemon(ourPokemon)}>
+          {" "}
+          Fight with {ourPokemon.name}{" "}
+        </button>
+      </div>
+    );
+  });
 
-function fightBattle(fighterPokemon, pokemon){
-  console.log('Battle Begins')
-  const fighters = [fighterPokemon, pokemon];
-  const firstAttacker = fighters[Math.floor(Math.random()*2)];
-  const secondAttacker = fighters.find(fighter => fighter !== firstAttacker)
-  setTimeout(() => console.log(`First Attacker: ${firstAttacker.name}`),3000)
-  setTimeout(() => console.log(`Second Attacker: ${secondAttacker.name}`),5000)
-  
-  setTimeout(() => {
+  function fightBattle(fighterPokemon, pokemon) {
+    const updatedURLs = [...usersPokemonsURL];
+    console.log("Battle Begins");
+    const fighters = [fighterPokemon, pokemon];
+    const firstAttacker = fighters[Math.floor(Math.random() * 2)];
+    const secondAttacker = fighters.find(
+      (fighter) => fighter !== firstAttacker
+    );
+
+    console.log(`First Attacker: ${firstAttacker.name}`);
+    console.log(`Second Attacker: ${secondAttacker.name}`);
+
     do {
-      secondAttacker.stats[0].base_stat -= ((((2/5+2)*firstAttacker.stats[1].base_stat*60/secondAttacker.stats[2].base_stat)/50)+2)*Math.floor(Math.random() * (255 - 217 + 1) + 217)/255;
-      console.log(secondAttacker.name, secondAttacker.stats[0]); 
+      secondAttacker.stats[0].base_stat -=
+        ((((2 / 5 + 2) * firstAttacker.stats[1].base_stat * 60) /
+          secondAttacker.stats[2].base_stat /
+          50 +
+          2) *
+          Math.floor(Math.random() * (255 - 217 + 1) + 217)) /
+        255;
+
       if (secondAttacker.stats[0].base_stat <= 0) {
-        console.log(`${secondAttacker.name} dead`)
-        return;
+       
+        console.log(`${secondAttacker.name} dead`);
+        if (secondAttacker === pokemon) {
+          updatedURLs.push(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
+          console.log(`${pokemon.name} added to pokemons`);
+        }
+        onBattleEnd(null);
+        setUsersPokemonsURL(updatedURLs);
       }
-      firstAttacker.stats[0].base_stat -= ((((2/5+2)*secondAttacker.stats[1].base_stat*60/firstAttacker.stats[2].base_stat)/50)+2)*Math.floor(Math.random() * (255 - 217 + 1) + 217)/255;
-      console.log(firstAttacker.name, firstAttacker.stats[0]); 
+      firstAttacker.stats[0].base_stat -=
+        ((((2 / 5 + 2) * secondAttacker.stats[1].base_stat * 60) /
+          firstAttacker.stats[2].base_stat /
+          50 +
+          2) *
+          Math.floor(Math.random() * (255 - 217 + 1) + 217)) /
+        255;
+
       if (firstAttacker.stats[0].base_stat <= 0) {
-        console.log(`${firstAttacker.name} dead`)
-        return;
+        console.log(`${firstAttacker.name} dead`);
+        if (firstAttacker === pokemon) {
+          updatedURLs.push(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
+          console.log(`${pokemon.name} added to pokemons`);
+        }
+        onBattleEnd(null);
+        setUsersPokemonsURL(updatedURLs);
       }
-      
-    } while (firstAttacker.stats[0].base_stat > 0 && secondAttacker.stats[0].base_stat > 0);
-}, 6000);
+    } while (
+      firstAttacker.stats[0].base_stat > 0 &&
+      secondAttacker.stats[0].base_stat > 0
+    );
+    
+  }
 
-setTimeout(()=> onBattleEnd(null), 10000)
-  
-}
-
-    return (
-        <>
-        <div className="ourSide">
-        
-        {(!fighterPokemon) ?
-        
-        <>
-        <h1>Choose your fighter Pokemon!</h1> 
-        <div className="pokemonChooser">{usersPokemonsDisplay}</div>
-        </> :
-        (fightBattle(fighterPokemon, pokemon),
-        <div className="fighterPokemon">
-          <div className={getTypeClassNameOrImg(true, fighterPokemon.types[0].type.name)}>
-            <table className='nameheaders'>
-              <tr>
-                <td className='basic' colSpan={3}>Basic Pokemon</td>
-              </tr>
-              <tr>
-                <td className='pokemon-name'>{fighterPokemon.name}</td>
-                <td className='pokemon-hp'>HP{fighterPokemon.stats[0].base_stat}</td>
-                <td className='pokemon-type-icon'><img src={getTypeClassNameOrImg(false, fighterPokemon.types[0].type.name)} alt="" width={30} height={30} /></td>
-              </tr>
-            </table>
-            <img className='headshot' src={fighterPokemon.sprites.other['official-artwork'].front_shiny} alt={fighterPokemon.name} />
-            <p className='description'>asd. Length: {fighterPokemon.height * 10}CM Weight: {fighterPokemon.weight / 10}KG</p>
-            <div className='infoonly'>
-              <table className='stats'>
-                <tr>
-                  <td><span className='label'>Attack</span> <span className='labeltext'> </span></td>
-                  <td className='damage'>{fighterPokemon.stats[1].base_stat}</td>
-                </tr>
-              </table>
-              <hr />
-              <table className='stats'>
-                <tr>
-                  <td><span className='label'>Defense</span> <span className='labeltext'> </span></td>
-                  <td className='damage'>{fighterPokemon.stats[3].base_stat}</td>
-                </tr>
-              </table>
+  return (
+    <>
+      <div className="ourSide">
+        {!fighterPokemon ? (
+          <>
+            <h1>Choose your fighter Pokemon!</h1>
+            <div className="pokemonChooser">{usersPokemonsDisplay}</div>
+          </>
+        ) : (
+          (fightBattle(fighterPokemon, pokemon),
+          (
+            <div className="fighterPokemon">
+              <div
+                className={getTypeClassNameOrImg(
+                  true,
+                  fighterPokemon.types[0].type.name
+                )}
+              >
+                <table className="nameheaders">
+                  <tr>
+                    <td className="basic" colSpan={3}>
+                      Basic Pokemon
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="pokemon-name">{fighterPokemon.name}</td>
+                    <td className="pokemon-hp">
+                      HP{fighterPokemon.stats[0].base_stat}
+                    </td>
+                    <td className="pokemon-type-icon">
+                      <img
+                        src={getTypeClassNameOrImg(
+                          false,
+                          fighterPokemon.types[0].type.name
+                        )}
+                        alt=""
+                        width={30}
+                        height={30}
+                      />
+                    </td>
+                  </tr>
+                </table>
+                <img
+                  className="headshot"
+                  src={
+                    fighterPokemon.sprites.other["official-artwork"].front_shiny
+                  }
+                  alt={fighterPokemon.name}
+                />
+                <p className="description">
+                  asd. Length: {fighterPokemon.height * 10}CM Weight:{" "}
+                  {fighterPokemon.weight / 10}KG
+                </p>
+                <div className="infoonly">
+                  <table className="stats">
+                    <tr>
+                      <td>
+                        <span className="label">Attack</span>{" "}
+                        <span className="labeltext"> </span>
+                      </td>
+                      <td className="damage">
+                        {fighterPokemon.stats[1].base_stat}
+                      </td>
+                    </tr>
+                  </table>
+                  <hr />
+                  <table className="stats">
+                    <tr>
+                      <td>
+                        <span className="label">Defense</span>{" "}
+                        <span className="labeltext"> </span>
+                      </td>
+                      <td className="damage">
+                        {fighterPokemon.stats[3].base_stat}
+                      </td>
+                    </tr>
+                  </table>
+                </div>
+                <p className="italicize">asd</p>
+                <p className="bottomtext">
+                  <strong>Illus. Mitsuhiro Arita</strong> C 1995, 96, 98
+                  Nintendo Creatures, GAMEFREAK C 1999 Wizards.{" "}
+                  <strong>58/102 o</strong>
+                </p>
+              </div>
             </div>
-            <p className='italicize'>asd</p>
-            <p className='bottomtext'><strong>Illus. Mitsuhiro Arita</strong>  C 1995, 96, 98 Nintendo Creatures, GAMEFREAK C 1999 Wizards.  <strong>58/102 o</strong></p>
-          </div>
-        </div>)
-        } 
-        </div>
-        </>
-    )
+          ))
+        )}
+      </div>
+    </>
+  );
 }
